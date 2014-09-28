ed985e99b81da94df54f03d7439b04979479438b
Chris Chapman <chris@pentandra.com>
2014-09-27 23:40:50 -0600

    Reintroduce specs

---
+++ js/respec-w3c-extensions.js
@@ -0,0 +1,5 @@
+// extend the bibliographic entries
+var localBibliography = {
+  "HYDRA-CORE": "Markus Lanthaler. <cite><a href=\"http://www.hydra-cg.com/spec/latest/core/\">Hydra Core Vocabulary</a>.</cite> Unofficial Draft. URL: <a href=\"http://www.hydra-cg.com/spec/latest/core/\">http://www.hydra-cg.com/spec/latest/core/</a>.",
+    "PAV": "Paolo Ciccarese, Stian Soiland-Reyes, Khalid Belhajjame, Alasdair JG Gray, Carole Goble, Tim Clark (2013): <cite><a href=\"http://purl.org/pav/\">PAV ontology: provenance, authoring and versioning</a>.</cite> Journal of biomedical semantics 4:37. doi:<a href=\"http://dx.doi.org/10.1186/2041-1480-4-37\">10.1186/2041-1480-4-37</a>"
+};
