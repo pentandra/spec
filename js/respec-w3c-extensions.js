007c630953c76a27b74b46b48b20dcb8b3d79cae
Chris Chapman <chris@pentandra.com>
2014-09-29 17:20:27 -0600

    Set up localBiblio according to ReSpec's documentation:
    http://www.w3.org/respec/ref.html#localbiblio

--- js/respec-w3c-extensions.js
+++ js/respec-w3c-extensions.js
@@ -4,8 +4,21 @@ var localBibliography = {
     authors: [ "Markus Lanthaler" ],
     href: "http://www.hydra-cg.com/spec/latest/core/",
     title: "Hydra Core Vocabulary",
-    status: "unofficial",
+    status: "Unofficial Draft",
     publisher: "W3C"
   },
-    "PAV": "Paolo Ciccarese, Stian Soiland-Reyes, Khalid Belhajjame, Alasdair JG Gray, Carole Goble, Tim Clark (2013): <cite><a href=\"http://purl.org/pav/\">PAV ontology: provenance, authoring and versioning</a>.</cite> Journal of biomedical semantics 4:37. doi:<a href=\"http://dx.doi.org/10.1186/2041-1480-4-37\">10.1186/2041-1480-4-37</a>"
+  "PAV": {
+    authors: [
+      "Paolo Ciccarese",
+      "Stian Soiland-Reyes",
+      "Khalid Belhajjame",
+      "Alasdair JG Gray",
+      "Carole Goble",
+      "Tim Clark"
+    ],
+    href: "http://purl.org/pav/",
+    title: "PAV ontology: provenance, authoring and versioning",
+    publisher: "Journal of biomedical semantics",
+    status: "Published"
+  }
 };
