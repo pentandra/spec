3fefe8c7d9dcca31c1f1866cc49843e030d32787
Chris Chapman <chris@pentandra.com>
2015-11-21 22:29:10 -0700

    Added link to the micropublications paper

--- js/respec-w3c-extensions.js
+++ js/respec-w3c-extensions.js
@@ -20,5 +20,16 @@ var localBibliography = {
     title: "PAV ontology: provenance, authoring and versioning",
     publisher: "Journal of biomedical semantics",
     status: "Published"
-  }
+  },
+  "mp": {
+    authors: [
+      "Tim Clark",
+      "Paolo Ciccarese",
+      "Carole Goble"
+    ],
+    href: "http://www.jbiomedsem.com/content/5/1/28",
+    title: "Micropublications: a semantic model for claims, evidence, arguments and annotations in biomedical communications",
+    publisher: "Journal of biomedical semantics",
+    status: "Published"
+  },
 };
